92374c55d5207304c13d75a50f0111809f0ad6c6
abredo <1468266140@qq.com>
2016-03-17 11:14:00 +0800

    Update ics.sh

--- ics.sh
+++ ics.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 filename="icon.png"
-current=`date +%Y-%m-%d_%H:%M:%S`
-dirname=ResizedIcons_${current}
+current_time=`date +%Y-%m-%d_%H:%M:%S`
+dirname=ResizedIcons_${current_time}
 name_array=("Icon-29.png" "Icon-29@2x.png" "Icon-29@3x.png" "Icon-40.png" "Icon-40@2x.png" "Icon-40@3x.png" "Icon-60@2x.png" "Icon-60@3x.png" "Icon-76.png" "Icon-76@2x.png" "Icon-83.5@2x.png")
 size_array=("29" "58" "87" "40" "80" "120" "120" "180" "76" "152" "167")
 
